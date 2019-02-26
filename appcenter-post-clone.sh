2c4595046c001855b19a674f0948fd3ffa806519
Ram Ganesan <ramg8x@gmail.com>
2019-02-26 09:46:04 +0530

    jniLibs

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -10,9 +10,9 @@ echo "unzipping OpenCV"
 unzip "$APPCENTER_SOURCE_DIRECTORY/deps/opencv.zip"
 # echo "moving the JNI directory"
 # mv "$APPCENTER_SOURCE_DIRECTORY/opencv/" "$APPCENTER_SOURCE_DIRECTORY/deps/opencv/"
-ls "$APPCENTER_SOURCE_DIRECTORY/deps/opencv"
-# rm "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/jniLibs"
-# ln -s "$APPCENTER_SOURCE_DIRECTORY/deps/opencv/sdk/native/libs" "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/jniLibs"
+# ls "$APPCENTER_SOURCE_DIRECTORY/deps/opencv/x86"
+rm "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/jniLibs"
+ln -s "$APPCENTER_SOURCE_DIRECTORY/deps/opencv/sdk/native/libs" "$APPCENTER_SOURCE_DIRECTORY/android/app/src/main/jniLibs"
 
 
 echo "Post clone script. Done."
